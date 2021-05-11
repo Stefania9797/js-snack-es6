b6b3ad574a487d9e8dde7b3bf8cebc976c3f1da4
Stefania9797 <stefaniamastrel@gmail.com>
2021-05-11 15:23:35 +0200

    create new array

--- snack-2/main.js
+++ snack-2/main.js
@@ -1,8 +1,34 @@
 /*Snack2:
 Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà:
-nome,
-punti fatti,
-falli subiti.
+nome, punti fatti,falli subiti.
 Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti.
 Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.*/
+
+let squadre=[
+    {nome:"Juventus",puntiFatti:0,falliSubiti:0},
+    {nome:"Inter",puntiFatti:0,falliSubiti:0},
+    {nome:"Milan",puntiFatti:0,falliSubiti:0},
+    {nome:"Roma",puntiFatti:0,falliSubiti:0},
+]
+//funzione che genera numeri casuali
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
+//generare numeri random per punti e falli degli oggetti dell'array
+for (let index = 0; index < squadre.length; index++) {
+    squadre[index].puntiFatti=getRandomNumber(1,100);
+    squadre[index].falliSubiti=getRandomNumber(1,100);
+}
+console.log(squadre);
+//Infine creiamo un nuovo array i cui elementi contengono 
+//solo nomi e falli subiti e stampiamo tutto in console.
+let newSquadre=[];
+for (let index = 0; index < squadre.length; index++) {
+    let {nome,falliSubiti}=squadre[index]
+    newSquadre.push(squadre[index].nome, squadre[index].falliSubiti)
+}
+console.log(newSquadre);
+
+
+
